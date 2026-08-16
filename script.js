90522e662bf288f11b265afe9cd078feddc00d6e
mrtomicz <git@mrtomicz.eu>
2026-08-16 13:22:18 +0200

    rm dbg & add guide etc

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ console.log("bp1");
 
   // Get hash value of current address bar
   if (window.location.hash) {
-    console.log("bp21");
     // Decode hash value in case it's non-ASCII
     payload = decodeURIComponent(window.location.hash.slice(1));
     // Remove all whitespace - we never use whitespace when encoding hash values
@@ -21,32 +20,26 @@ console.log("bp1");
     const useEmoji = Array.from(payload).some(c => !outputAlphabetASCII.includes(c));
     alphabet = useEmoji ? outputAlphabetEmoji : outputAlphabetASCII;
   } else {
-    console.log("bp22");
     // If no hash value, we're likely reading a QR code
     // For that, use the path instead
     payload = decodeURIComponent(window.location.pathname.slice(1));
     alphabet = outputAlphabetQR;
   }
-console.log("bp3");
+
   if (payload && payload.trim()) {
-    console.log("bp41");
     try {
-      console.log("bp411");
       const target = decompress(payload, alphabet);
 //      window.location.href = target;
       //alert(target);
       decoded.innerText = target;
       return;
     } catch (e) {
-      console.log("bp412");
 //      console.warn(`Redirect failed. Could not decode input.`);
-      decoded.innerText = "Couldn't decode";
-      decoded.classList.remove("decoded");
-      decoded.classList.add("error");
+      cannotDecode();
       console.error(e);
     }
   } else {
-    console.log("bp42");
+    cannotDecode();
   }
 
 //  updateOutput();
@@ -57,3 +50,9 @@ console.log("bp3");
 
 })();
 
+
+function cannotDecode() {
+  decoded.innerHTML = "Couldn't decode<br>Syntax: append .mtmi.eu after a ha.mr URL to get it's destination for example<br>http://ha.mr#OM( -> http://ha.mr.mtmi.eu#OM(";
+  decoded.classList.remove("decoded");
+  decoded.classList.add("error");
+}
